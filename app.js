17bd655c93102adf1ea208174dd2c7ba2e705be8
casteser <61086379+SCASTELLANO6044@users.noreply.github.com>
2022-02-27 16:47:58 +0100

    adding ejs to the project

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 const express = require('express');
 
 //express app
-
 const app = express();
+
+//register view engine
+app.set('view engine','ejs');
 
 //listen for request
 app.listen(3000);
 
 app.get('/',(req,res)=>{
-    res.sendFile('./views/index.html',{root: __dirname});
+    res.render('index');
+    //res.sendFile('./views/index.html',{root: __dirname});
     //res.send('<h1>This is home page</h1>');
 });
 
 app.get('/about',(req,res)=>{
-    res.sendFile('./views/about.html',{root: __dirname});
+    res.render('about');
+    //res.sendFile('./views/about.html',{root: __dirname});
     //res.send('<h1>This is home page</h1>');
 });
 
+app.get('/blogs/create',(req, res)=>{
+    res.render('create')
+})
+
+/*
 //redirects
 app.get('/about-us',(req,res)=>{
     res.redirect('/about');
 })
+*/
 //404
 app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html',{root: __dirname});
+    res.status(404).render('404');
 })
